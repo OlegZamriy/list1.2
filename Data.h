9851f2aa47c47411369908d9d604944924c7ab94
Замрій Олег Володимирович <143324688+OlegZamriy@users.noreply.github.com>
2023-12-05 20:19:02 +0200

    Add project files.

---
+++ Data.h
@@ -0,0 +1,14 @@
+#ifndef DATA_H
+#define DATA_H
+
+#include <string>
+#include <vector>
+
+struct Task {
+    std::string title;
+    int priority;
+    std::string description;
+    std::string dateTime;
+};
+
+#endif // DATA_H
---
+++ Functions.h
@@ -0,0 +1,24 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include "Data.h"
+#include "Repository.h"
+
+class TaskManager {
+public:
+    void addTask();
+    void deleteTask();
+    void editTask();
+    void searchTasks();
+    void displayTasksByTime();
+    void displayTasksByPriority();
+
+private:
+    TaskRepository taskRepository;
+
+    void displayTasks(const std::vector<Task>& tasks);
+};
+
+void displayMenu();
+
+#endif // FUNCTIONS_H
